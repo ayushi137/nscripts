e13bb298368e37694cc16ef24c2e6646fedce726
NatalieP-J <natalie.price.jones@mail.utoronto.ca>
2015-10-06 11:26:06 -0400

    Streamlining and updating photometry

---
+++ correlate_config.py
@@ -0,0 +1,48 @@
+from astropy import units as u
+
+# specify telescope details
+scope_lat = {'Dragonfly':32.902836*u.deg}
+scope_long = {'Dragonfly':-105.528350*u.deg}
+scope_alt = {'Dragonfly':2214*u.m}
+
+# specify herschel resolution at each wavelength
+SPIRE = {'PSW':17.6,'PMW':23.9,'PLW':35.2}
+spirekeys = SPIRE.keys()
+
+config_data = {
+	
+	'scope':'Dragonfly'
+	'objects':['PGM_1_2','spi1_1']
+	'raws':'/raw_lights/'
+	'cals':'/calframes/'
+	'dsff':'/darksub_flatfield/'
+	'rwcorrfiles':True
+	'badframesdir':'/removedframes'
+}
+
+# OUTPUT DIRECTORIES
+
+# master dark location
+damdir = '/dark_masters/'
+# master flat location
+flmdir = '/flat_masters/'
+# dark subtracted files
+dsmdir = '/dark_subtracted/'
+
+# source extractor object maps
+objdir = '/objects/'
+# source extractor background maps
+bakdir = '/background/'
+# source extractor catalogue
+catdir = '/catalogue/'
+
+# location of helper plots from zero point magnitude calculations
+magdir = '/magnitudecalcs/'
+# location of photometered fits files
+phodir = '/photometered/'
+# location of regridded images
+regdir = '/regrid/'
+# location of background plane subtracted images
+bsudir = '/backgroundsub/'
+# location of correlation plots
+cordir = '/correlations/'
